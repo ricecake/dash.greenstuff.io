47d20b32e5473d041abcca00cbbbb6f5439d578d
Sebastian Green-Husted <geoffcake@gmail.com>
2020-03-24 19:12:43 -0400

    stuff

---
+++ source/includes/config.js
@@ -0,0 +1,37 @@
+import deepmerge from 'deepmerge';
+
+const common = {
+	identity: {
+		response_type: 'code',
+		scope: 'openid profile',
+		oidc_path: '/oauth',
+		automaticSilentRenew:true,
+		validateSubOnSilentRenew: true,
+		loadUserInfo: false,
+	},
+};
+const dev = {
+	hosts: {
+		idp_path: 'https://login.devhost.dev',
+		app_path: 'https://dash.devhost.dev'
+	},
+	identity: {
+		client_id: '-6ouqjRZT3WF191MmKCVtQ',
+	},
+};
+const production = {
+	hosts: {
+		idp_path: 'https://login.greenstuff.io',
+		app_path: 'https://dash.greenstuff.io'
+	},
+	identity: {
+		client_id: 'INVALID',
+	},
+};
+
+const MergedConfig = deepmerge.all([
+	common,
+	(process.env.production? production : dev),
+]);
+
+export default MergedConfig;
